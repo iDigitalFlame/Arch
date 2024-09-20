294f1bfd88e1bc1e67cc3adccba2e5883ba14007
iDigitalFlame <idf@idigitalflame.com>
2024-09-20 16:25:59 -0400

    Updated to fix alpm ownership

--- Config/etc/syscheck.d/cache.sh
+++ Config/etc/syscheck.d/cache.sh
@@ -11,8 +11,8 @@ chmod 0644 "/var/cache/ld.so.cache"
 chown root:root "/var/cache/ld.so.cache"
 
 chmod -R 0750 "/var/cache/pacman/pkg"
-chmod 0640 /var/cache/pacman/pkg/*
-chown -R root:root "/var/cache/pacman/pkg"
+find "/var/cache/pacman/pkg" -type f -exec chmod 0640 {} \;
+chown -R root:alpm "/var/cache/pacman/pkg"
 
 chmod 0755 "/var/lib/pacman/sync"
 chmod 0644 /var/lib/pacman/sync/*
